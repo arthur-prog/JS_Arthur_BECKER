5be45d9d2d7b2c8d3fb9198479f7f7dcca12e1cd
arthur-prog <arthurbecker57420@gmail.com>
2022-10-23 15:21:56 +0200

    'modif'

--- TD5/js/ct.js
+++ TD5/js/ct.js
@@ -43,10 +43,10 @@ async function getData(pagination) {
     let nbRows = "10";
     let start = pagination * 10;
     if(ville === undefined){
-        url = "https://data.economie.gouv.fr/api/records/1.0/search/?dataset=prix-des-controles-techniques-copie&q=&rows=" + nbRows + "&start=" + (start-1) + "&facet=cct_denomination&facet=prix_visite&facet=prix_contre_visite_min&facet=prix_contre_visite_max&refine.cct_code_dept=" + dpt;
+        url = "https://data.economie.gouv.fr/api/records/1.0/search/?dataset=prix-des-controles-techniques-copie&q=&sort=cct_denomination&rows=" + nbRows + "&start=" + (start-1) + "&facet=cct_denomination&facet=prix_visite&facet=prix_contre_visite_min&facet=prix_contre_visite_max&refine.cct_code_dept=" + dpt;
     }
     else{
-        url = "https://data.economie.gouv.fr/api/records/1.0/search/?dataset=prix-des-controles-techniques-copie&q=&rows=" + nbRows + "&start=" + (start-1) + "&facet=cct_denomination&facet=prix_visite&facet=prix_contre_visite_min&facet=prix_contre_visite_max&refine.cct_code_commune=" + ville;
+        url = "https://data.economie.gouv.fr/api/records/1.0/search/?dataset=prix-des-controles-techniques-copie&q=&sort=cct_denomination&rows=" + nbRows + "&start=" + (start-1) + "&facet=cct_denomination&facet=prix_visite&facet=prix_contre_visite_min&facet=prix_contre_visite_max&refine.cct_code_commune=" + ville;
     }
     const response = await fetch(
         url , {
@@ -55,6 +55,8 @@ async function getData(pagination) {
         console.log("erreur:" + err);
     });
     const json = await response.json();
+
+    console.log(json);
 
     if(nbPages === undefined){
         nbPages = Math.ceil(json.nhits/nbRows);
@@ -73,11 +75,16 @@ async function getData(pagination) {
         let table = document.createElement("table");
         table.appendChild(initializeTable());
         json.records.forEach(centre =>{
+            console.log(centre);
             let tr = document.createElement("tr");
             let nom = document.createElement("td");
             nom.innerText = centre.fields.cct_denomination;
             let adresse = document.createElement('td');
             adresse.innerText = centre.fields.cct_adresse;
+            let categorie_vehicule = document.createElement('td');
+            categorie_vehicule.innerText = centre.fields.cat_vehicule_libelle;
+            let categorie_energie = document.createElement('td');
+            categorie_energie.innerText = centre.fields.cat_energie_libelle;
             let prix_visite = document.createElement('td');
             prix_visite.innerText = centre.fields.prix_visite;
             let prix_contre_visite_max = document.createElement('td');
@@ -86,7 +93,7 @@ async function getData(pagination) {
             prix_contre_visite_min.innerText = centre.fields.prix_contre_visite_min;
 
 
-            tr.append(nom, adresse, prix_visite, prix_contre_visite_max, prix_contre_visite_min);
+            tr.append(nom, adresse, categorie_vehicule, categorie_energie, prix_visite, prix_contre_visite_max, prix_contre_visite_min);
             table.appendChild(tr);
         });
         div_affichage.appendChild(table);
@@ -114,13 +121,17 @@ function initializeTable(){
     nom.innerText = "Nom";
     let adresse = document.createElement('th');
     adresse.innerText = "Adresse";
+    let categorie = document.createElement('th');
+    categorie.innerText = "Catégorie de véhicule";
+    let categorie_energie = document.createElement('th');
+    categorie_energie.innerText = "Energie";
     let prix_visite = document.createElement('th');
     prix_visite.innerText = "Prix visite";
     let prix_contre_visite_max = document.createElement('th');
     prix_contre_visite_max.innerText = "Prix max contre visite";
     let prix_contre_visite_min = document.createElement('th');
     prix_contre_visite_min.innerText = "Prix min contre visite";
-    tr.append(nom, adresse, prix_visite, prix_contre_visite_max, prix_contre_visite_min);
+    tr.append(nom, adresse, categorie, categorie_energie, prix_visite, prix_contre_visite_max, prix_contre_visite_min);
     return tr;
 }
 
